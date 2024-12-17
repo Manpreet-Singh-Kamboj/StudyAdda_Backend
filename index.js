533d884195ba8a5f2b62fe77d3478205653fac94
Manpreet Singh <97432223+Manpreet3033@users.noreply.github.com>
2024-12-17 14:44:47 +0530

    Some Fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const serverless = require("serverless-http");
 const userRoutes = require("./api/routes/User");
 const profileRoutes = require("./api/routes/Profile");
 const paymentRoutes = require("./api/routes/Payments");
@@ -52,6 +52,4 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`App is running at ${PORT}`);
-});
+module.exports.handler = serverless(app);
